86c49860aa73a44273078fbf0b01b4f22a8dfa79
annjhwang <annhwang@college.harvard.edu>
2016-12-04 15:01:49 -0500

    added more restaurants

--- knapsack.py
+++ knapsack.py
@@ -11,55 +11,55 @@ max_calories = input('Enter the maximum amount of calories: ')
 
 def totalvalue(comb):
     ' Totalise a particular combination of items'
-    totwt = totval = 0
-    for item, wt, val in comb:
-        totwt  += wt
+    tot_calories = totval = 0
+    for item, calories, val in comb:
+        tot_calories  += calories
         totval += val
-    return (totval, -totwt) if totwt <= max_calories else (0, 0)
- 
-items = pd.DataFrame.from_csv('burgerkingdata.csv')
-items = items.drop('Food Category', 1)
-items = items.drop('Item Description 2015', 1)
-items = items.drop('Servings Size Text 2015', 1)
-items = items.drop('Menu_Item_ID', 1)
-items = items.drop('Serving Size Unit 2015', 1)
-items = items.drop('Servings Per Item 2015', 1)
+    return (totval, -tot_calories) if tot_calories <= max_calories else (0, 0)
 
-names = items['Item_Name'].values
-calories = items['Calories 2015'].values
-protein = items['Protein (g) 2015'].values
-items = zip(names, calories, protein)
+# importing all the data and cleaning
+burgerking = pd.DataFrame.from_csv('burgerkingdata.csv')
+elpollo = pd.DataFrame.from_csv('elpollodata.csv')
+chickfila = pd.DataFrame.from_csv('chickfiladata.csv')
+tacobell = pd.DataFrame.from_csv('tacobell.csv')
+subway= pd.DataFrame.from_csv('subway.csv')
 
-print type(items)
- 
-def knapsack01_dp(items, limit):
-    table = [[0 for w in range(limit + 1)] for j in xrange(len(items) + 1)]
- 
-    for j in xrange(1, len(items) + 1):
-        item, wt, val = items[j-1]
+
+def knapsack(foods, limit):
+    table = [[0 for w in range(limit + 1)] for j in xrange(len(foods) + 1)]
+    for j in xrange(1, len(foods) + 1):
+        food, calories, val = foods[j-1]
         for w in xrange(1, limit + 1):
-            if wt > w:
+            if calories > w:
                 table[j][w] = table[j-1][w]
             else:
                 table[j][w] = max(table[j-1][w],
-                                  table[j-1][w-wt] + val)
+                                  table[j-1][w-calories] + val)
  
-    result = []
+    best_foods = []
     w = limit
-    for j in range(len(items), 0, -1):
+    for j in range(len(foods), 0, -1):
         was_added = table[j][w] != table[j-1][w]
  
         if was_added:
-            item, wt, val = items[j-1]
-            result.append(items[j-1])
-            w -= wt
+            food, calories, val = foods[j-1]
+            best_foods.append(foods[j-1])
+            w -= calories
  
-    return result
+    return best_foods
  
- 
-bagged = knapsack01_dp(items, max_calories)
-print bagged
-print("Bagged the following items\n  " +
-      '\n  '.join(sorted(item for item,_,_ in bagged)))
-val, wt = totalvalue(bagged)
-print("for a total grams of protein of %i and a total caloric intake of %i" % (val, -wt))
+restaurants = [burgerking, elpollo, chickfila, tacobell, subway]
+restaurant_names = ['Burger King', 'El Pollo Loco', 'Chick-Fila-A' , 'Taco Bell', 'Subway']
+
+for restaurant, name in zip(restaurants, restaurant_names):
+    names = restaurant['Item_Name'].values
+    calories = restaurant['Calories 2015'].values
+    protein = restaurant['Protein (g) 2015'].values
+    items = zip(names, calories, protein)
+
+    # knapsack problem for burgerking 
+    bagged = knapsack(items, max_calories)
+    print("Bagged the following food items from " + name + ":\n  " +
+          '\n  '.join(sorted(item for item,_,_ in bagged)))
+    val, calories = totalvalue(bagged)
+    print("Total grams of protein of %i and a total caloric intake of %i" % (val, -calories)) 
