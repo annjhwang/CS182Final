d01388ea1c66fc4d52999389b6ef777af08e3810
annjhwang <annhwang@college.harvard.edu>
2016-12-05 23:18:13 -0500

    basic flask framework

--- knapsack.py
+++ knapsack.py
@@ -4,25 +4,14 @@
 # Annie Hwang | annhwang@college.harvard.edu
 import pandas as pd
 import numpy as np
-
-try:
-    xrange
-except:
-    xrange = range
+from itertools import combinations
+import time
  
+
 # allows user to input maximum caloric intake
 max_calories = input('Enter the maximum amount of calories: ')
 
-def totalvalue(comb):
-    ' Totalise a particular combination of items'
-    tot_calories = totval = 0
-    for item, calories, val in comb:
-        tot_calories  += calories
-        totval += val
-    return (totval, -tot_calories) if tot_calories <= max_calories else (0, 0)
-
 # importing all the data and cleaning
-<<<<<<< HEAD
 burgerking = pd.DataFrame.from_csv('burgerking.csv')
 elpollo = pd.DataFrame.from_csv('elpollo.csv')
 chickfila = pd.DataFrame.from_csv('chickfila.csv')
@@ -33,15 +22,27 @@ pandaexpress = pd.DataFrame.from_csv('pandaexpress.csv')
 panera = pd.DataFrame.from_csv('panera.csv')
 carlsjr = pd.DataFrame.from_csv('carlsjr.csv')
 kfc = pd.DataFrame.from_csv('kfc.csv')
-=======
-burgerking = pd.DataFrame.from_csv('burgerkingdata.csv')
-elpollo = pd.DataFrame.from_csv('elpollodata.csv')
-chickfila = pd.DataFrame.from_csv('chickfiladata.csv')
-tacobell = pd.DataFrame.from_csv('tacobell.csv')
-subway= pd.DataFrame.from_csv('subway.csv')
->>>>>>> fadfa7047129de1572e89d26c0bb142fbeef917f
 
-def knapsack(foods, limit):
+restaurants = [burgerking, elpollo, chickfila, tacobell, subway, abp, pandaexpress, panera, carlsjr, kfc]
+restaurant_names = ['Burger King', 'El Pollo Loco', 'Chick-Fila-A' , 'Taco Bell', 'Subway', 'Au Bon Pain', 'Panda Express', 'Panera Bread', 'Carls Jr', 'KFC']
+
+def totalvalue(comb):
+    tot_calories = totval = 0
+    for item, calories, val in comb:
+        tot_calories  += calories
+        totval += val
+    return (totval, -tot_calories) if tot_calories <= max_calories else (0, 0)
+
+# BRUTE FORCE APPROACH 
+def knapsack_brute_force(items):
+    # brute force approach that just tries every combination possible
+    return ( comb
+             for r in range(1, len(items)+1)
+             for comb in combinations(items, r)
+             )
+ 
+# DYNAMIC PROGRAMMING APPROACH
+def knapsack_dp(foods, limit):
     table = [[0 for w in range(limit + 1)] for j in xrange(len(foods) + 1)]
     for j in xrange(1, len(foods) + 1):
         food, calories, val = foods[j-1]
@@ -61,10 +62,10 @@ def knapsack(foods, limit):
             best_foods.append(foods[j-1])
             w -= calories
     return best_foods
- 
-restaurants = [burgerking, elpollo, chickfila, tacobell, subway, abp, pandaexpress, panera, carlsjr, kfc]
-restaurant_names = ['Burger King', 'El Pollo Loco', 'Chick-Fila-A' , 'Taco Bell', 'Subway', 'Au Bon Pain', 'Panda Express', 'Panera Bread', 'Carls Jr', 'KFC']
 
+# TIME BRUTE FORCE APPROACH FOR 10 RESTAURANTS
+print 'Brute Force Approach:'
+start_time = time.time()
 for restaurant, name in zip(restaurants, restaurant_names):
     names = restaurant['Item_Name'].values
     calories = restaurant['Calories 2015'].values
@@ -72,8 +73,28 @@ for restaurant, name in zip(restaurants, restaurant_names):
     items = zip(names, calories, protein)
 
     # knapsack problem for burgerking 
-    bagged = knapsack(items, max_calories)
+    bagged = max(knapsack_brute_force(items), key=totalvalue)
     print("Bagged the following food items from " + name + ":\n  " +
           '\n  '.join(sorted(item for item,_,_ in bagged)))
     val, calories = totalvalue(bagged)
     print("Total grams of protein of %i and a total caloric intake of %i" % (val, -calories)) 
+end_time = time.time()
+print("--- %s seconds ---" % (end_time - start_time))
+
+# TIME DP APPROACH FOR 10 RESTAURANTS 
+print 'Dynamic Programming Approach:'
+start_time = time.time()
+for restaurant, name in zip(restaurants, restaurant_names):
+    names = restaurant['Item_Name'].values
+    calories = restaurant['Calories 2015'].values
+    protein = restaurant['Protein (g) 2015'].values
+    items = zip(names, calories, protein)
+
+    # knapsack problem for burgerking 
+    bagged = knapsack_dp(items, max_calories)
+    print("Bagged the following food items from " + name + ":\n  " +
+          '\n  '.join(sorted(item for item,_,_ in bagged)))
+    val, calories = totalvalue(bagged)
+    print("Total grams of protein of %i and a total caloric intake of %i" % (val, -calories)) 
+end_time = time.time()
+print("--- %s seconds ---" % (end_time - start_time))
